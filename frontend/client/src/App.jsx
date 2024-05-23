15184e52d87202446bcc64b4aedcdb86968a1d67
Kingsley Onuoha <onuohakingsley521@gmail.com>
2024-05-23 20:40:56 +0100

    Added routes and created pages

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/SignUp";
+import Error from "./pages/Error";
+import About from "./pages/About";
 
 const App = () => {
   return (
-    <div className=' text-red-600'>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;

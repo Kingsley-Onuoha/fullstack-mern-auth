d641e6afbf6b7a65233c17751d7c7fff86b727c4
Kingsley Onuoha <onuohakingsley521@gmail.com>
2024-05-30 17:00:11 +0100

    create api test route

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "../routes/user.routes.js";
+
 dotenv.config();
 
 mongoose
-  .connect(
-    process.env.MONGO_URI
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -19,3 +19,6 @@ const app = express();
 app.listen(3000, () => {
   console.log("server is running on PORT 3000");
 });
+
+// configuring the user router
+app.use("/api/user", userRoutes);

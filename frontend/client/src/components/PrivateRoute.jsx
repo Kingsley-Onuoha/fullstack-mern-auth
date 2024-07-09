4d02e84d8928cec7a6e32a76fb1c0a456f57d7e3
Kingsley Onuoha <onuohakingsley521@gmail.com>
2024-07-09 16:34:35 +0100

    completed profile page UI

--- frontend/client/src/components/PrivateRoute.jsx
+++ frontend/client/src/components/PrivateRoute.jsx
@@ -1,12 +1,10 @@
-import { useSelector } from "react-redux"
-import { Navigate, Outlet } from "react-router-dom"
-import Profile from "../pages/Profile"
-
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import Profile from "../pages/Profile";
 
 const PrivateRoute = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Profile /> : <Navigate to="/signin" />;
+};
 
-    const  {currentUser}  = useSelector(state => state.user) 
-  return currentUser? <Profile />: <Navigate to="/signin"/>
-}
-
-export default PrivateRoute
+export default PrivateRoute;
--- frontend/client/src/pages/Profile.jsx
+++ frontend/client/src/pages/Profile.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
-const Profile = () => {
+ const Profile = () => {
+   const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <div>Hello World</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
 
-export default Profile
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.profilePhoto}
+          alt="profile"
+          className="h-24 w-24 mt-1 self-center cursor-pointer rounded-full object-cover"
+        />
+
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="bg-slate-100 p-3 rounded-lg"
+          defaultValue={currentUser.username}
+        />
+
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="bg-slate-100 p-3 rounded-lg"
+          defaultValue={currentUser.email}
+        />
+
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:bg-opacity-80">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
